a6e579421149750816b0eee7b828391083cc24f0
Khalil Stroman <7208392@philasd.org>
2020-08-29 02:07:02 +0000

    capital letter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 temp = input("Enter temperature in celsius: ")
 temp = int(temp)
 far = (temp *1.8) + 32
-print(str(temp) + ".0° in celsius is equivalent to " + str(far) + "° Fahrenheit.")
+print(str(temp) + ".0° in Celsius is equivalent to " + str(far) + "° Fahrenheit.")
